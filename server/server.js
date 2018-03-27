1c1230769722ccd3a4d397c39eb760d19726b580
Grigor P. Penev <Grigor.Penev@verint.com>
2018-03-27 17:14:23 +0300

    Added PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
-var express = require("express");
-var bodyParser = require("body-parser");
+const express = require("express");
+const bodyParser = require("body-parser");
+const _ = require("lodash");
 
 var { mongoose } = require("./db/mongoose");
 var { Todo } = require("./models/todo");
@@ -35,12 +36,12 @@ app.get("/todos/:id", (req, res) => {
     var id = req.params.id;
 
     if (!ObjectID.isValid(id)) {
-        res.status(404).send({error: "Not a valid ID"});
+        return res.status(404).send({error: "Not a valid ID"});
     }
 
     Todo.findById(id).then((todo) => {
         if (!todo) {
-            res.status(404).send({error: "Todo not found"});
+            return res.status(404).send({error: "Todo not found"});
         }
 
         res.send({todo});
@@ -51,16 +52,49 @@ app.delete("/todos/:id", (req, res) => {
     var id = req.params.id;
 
     if (!ObjectID.isValid(id)) {
-        res.status(404).send({error: "Not a valid ID"});
+        return res.status(404).send({error: "Not a valid ID"});
     }
 
     Todo.findByIdAndRemove(id).then((todo) => {
         if (!todo) {
-            res.status(404).send({error: "Todo not found"});
+            return res.status(404).send({error: "Todo not found"});
         }
 
         res.send({todo});
     }).catch(e => res.status(400).send(e));
+});
+
+app.patch("/todos/:id", (req, res) => {
+    var id = req.params.id;
+
+    var body = _.pick(req.body, ["text", "completed"]);
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send({error: "Not a valid ID"});
+    }
+
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {
+        $set: body
+    }, {
+        new: true
+    })
+    .then((todo) => {
+        if (!todo) {
+            return res.send(404).send({error: "Todo not found"});
+        }
+
+        res.send({todo});
+
+    }).catch(e => {
+        res.status(400).send(e)
+    });
 });
 
 app.listen(3000, () => {
