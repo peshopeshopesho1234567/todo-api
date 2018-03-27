24c7f37a5a07f6e08cd7544de47dc57916ef6f70
Grigor P. Penev <Grigor.Penev@verint.com>
2018-03-27 18:14:19 +0300

    Set separetd test and development env's

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ var mongoose = require("mongoose");
 var { Schema } = mongoose;
 
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost:27017/TodoApp");
+mongoose.connect(process.env.MONGODB_URI);
 
 module.export = {
     mongoose
